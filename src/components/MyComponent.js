9d9a5e079c7288773966ae022043afbce5754932
knee-son <okaokaloid@gmail.com>
2023-09-01 20:57:23 +0800

    installed tailwind, installed firebase

---
+++ src/components/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MyComponent() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      This is a component styled with Tailwind CSS.
+    </div>
+  );
+}
+
+export default MyComponent;
