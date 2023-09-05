dccbb635434284e24b2c7d3369191bb80abb8aa0
knee-son <okaokaloid@gmail.com>
2023-09-05 22:50:45 +0800

    test

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
       <div className="App">
           <div>
             <Login/>
           </div>
       </div>
-    </Router>
   );
 }
 
