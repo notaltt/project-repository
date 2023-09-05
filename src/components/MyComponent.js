bf2e35b6645f5caa62660254e08f448623c808a3
knee-son <okaokaloid@gmail.com>
2023-09-05 12:31:06 +0800

    login page

--- src/components/MyComponent.js
+++
@@ -1,7 +0,0 @@
-export default function MyComponent() {
-  return (
-    <div className="text-white p-4 bg-gray-900 text-white sticky bottom-0">
-      This is a component styled with Tailwind CSS.
-    </div>
-  );
-}
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,4 +1,4 @@
-export default function DarkPanel() {
+export default function Panel() {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
@@ -20,14 +20,14 @@ export default function DarkPanel() {
             <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
               Boost your productivity.
               <br />
-              Start using Level Zero today.
+              Start using Privo today.
             </h2>
             <p className="mt-6 text-lg leading-8 text-gray-300">
               Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
               <a
-                href="#"
+                href="/login"
                 className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
               >
                 Get started
