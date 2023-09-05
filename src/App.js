a8d156340cde73b46cf92780acb7e68e9887c3b1
QuadTarzan30 <padigoskentalison@gmail.com>
2023-09-05 23:35:42 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import Panel from "./components/Panel";
 import Login from "./components/Login";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
       <div className="App">
           <div>
-            <Login/>
+            <Routes>
+              <Route path="/" element={<Panel/>}>
+              </Route>
+              <Route path="/login" element={<Login/>}>
+              </Route>
+            </Routes>
           </div>
       </div>
+    </Router>
   );
 }
 
