ad50cf0886d4f2e62f8bb6a31de54e9aee44dee4
knee-son <okaokaloid@gmail.com>
2023-09-07 18:15:22 +0800

    home content

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,91 @@
 import React, { useState } from 'react';
 import myImage from '../images/logo.png';
 
+const callouts = [
+    {
+        name: 'Data Wranglers',
+        description: '6 members',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+        imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+        href: '#',
+    },
+    {
+        name: 'Information Navigators',
+        description: '4 members',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
+        imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+        href: '#',
+    },
+    {
+        name: 'Data Surgeons',
+        description: '7 members',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+        imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+        href: '#',
+    },
+]
+
+const people = [
+    {
+        name: 'Leslie Alexander',
+        message: 'Deleted readme.md',
+        team: "Data Surgeons",
+        role: 'Intern',
+        imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        lastSeen: '3h ago',
+        lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+        name: 'Michael Foster',
+        message: 'Added icon.png',
+        team: "Information Navigators",
+        role: 'Front-end Developer',
+        imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        lastSeen: '3h ago',
+        lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+        name: 'Dries Vincent',
+        message: 'Updated App.js',
+        team: "Data Wranglers",
+        role: 'Business Relations',
+        imageUrl:
+        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        lastSeen: '3m ago',
+    },
+    {
+        name: 'Lindsay Walton',
+        message: 'Added you to team \'Data Surgeons\'',
+        team: "Data Surgeons",
+        role: 'Manager',
+        imageUrl:
+        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        lastSeen: '3h ago',
+        lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+        name: 'Courtney Henry',
+        message: 'Added helloworld.txt',
+        team: "Information Navigators",
+        role: 'Designer',
+        imageUrl:
+        'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        lastSeen: '3h ago',
+        lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+        name: 'Tom Cook',
+        message: 'Created folder \'dataset\'',
+        team: "Data Wranglers",
+        role: 'Director of Product',
+        imageUrl:
+        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        lastSeen: '15m ago',
+    },
+]
+
 export default function Dashboard(){
     return(
         <div className="flex bg-white h-screen">  
@@ -8,28 +93,86 @@ export default function Dashboard(){
             <div className='class="flex flex-col flex-1 w-full"'>
                 <header className='z-10 py-4 bg-white shadow-md dark:bg-white'>
                     
-                    <div class="flex  flex-1 lg:mr-32">
+                    <div className="flex  flex-1 lg:mr-32">
                     <a className='mr-10 align-right flex items-center text-xl font-bold text-blue-800 dark:text-blue-500' href="/dashboard">
                             <img
                             className="ml-5 mr-5 h-6 w-auto "
                             src={myImage}/>
                             <h1>PRIVO</h1>
                         </a>
-                        <div class=" justify-content relative w-full max-w-xl mr-6 focus-within:text-purple-500">
-                            <div class="absolute inset-y-0 flex items-center pl-2">
-                            <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
+                        <div className=" justify-content relative w-full max-w-xl mr-6 focus-within:text-purple-500">
+                            <div className="absolute inset-y-0 flex items-center pl-2">
+                            <svg className="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                                <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path>
                             </svg>
                             </div>
-                            <input class="w-full pl-8 pr-2 text-large text-black placeholder-blue-600 bg-gray-200 border-0 rounded-md dark:placeholder-gray-500 dark:focus:shadow-outline-blue dark:focus:placeholder-gray-600 dark:bg-gray-200 dark:text-gray-200 focus:placeholder-gray-500 focus:bg-white focus:border-red-300 focus:outline-none focus:shadow-outline-purple focus:text-blue-500 form-input" type="text" placeholder="Search" aria-label="Search"></input>
+                            <input className="w-full pl-8 pr-2 text-large text-black placeholder-blue-600 bg-gray-200 border-0 rounded-md dark:placeholder-gray-500 dark:focus:shadow-outline-blue dark:focus:placeholder-gray-600 dark:bg-gray-200 dark:text-gray-200 focus:placeholder-gray-500 focus:bg-white focus:border-red-300 focus:outline-none focus:shadow-outline-purple focus:text-blue-500 form-input" type="text" placeholder="Search" aria-label="Search"></input>
                         </div>  
                     </div> 
                 </header>
                 <main>
                     {/* put code what is inside the content page */}
+                    <div className="bg-gray-100">
+                        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-12">
+                            <div className="mx-auto max-w-2xl py-8 sm:py-12 lg:max-w-none lg:py-8">
+                            <h2 className="text-3xl font-bold text-black-700 opacity-70">Teams You're In</h2>
+
+                            <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+                                {callouts.map((callout) => (
+                                <div key={callout.description} className="group relative">
+                                    <div className="relative h-70 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                                    <img
+                                        src={callout.imageSrc}
+                                        alt={callout.imageAlt}
+                                        className="h-full w-full object-cover object-center"
+                                    />
+                                    </div>
+                                    <h3 className="mt-6 text-sm text-gray-500">
+                                    <a href={callout.href}>
+                                        <span className="absolute inset-0" />
+                                        {callout.description}
+                                    </a>
+                                    </h3>
+                                    <p className="text-base font-semibold text-gray-900">{callout.name}</p>
+                                </div>
+                                ))}
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <ul role="list" className="divide-y divide-gray-100 px-6 bg-gray-100 mt-10">
+                            <h2 className="text-3xl font-bold text-black-700 opacity-70 py-8 sm:py-12 lg:py-8 border-b-2 border-gray-500">Notifications</h2>
+                        {people.map((person) => (
+                            <li key={person.message} className="flex justify-between gap-x-6 py-5">
+                            <div className="flex min-w-0 gap-x-4">
+                                <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt="" />
+                                <div className="min-w-0">
+                                <p className="text-sm text-start font-semibold leading-6 text-gray-900">{person.name}</p>
+                                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.message}</p>
+                                </div>
+                            </div>
+                            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                                <p className="text-sm leading-6 text-gray-900">{person.role}</p>
+                                {person.lastSeen ? (
+                                <p className="mt-1 text-xs leading-5 text-gray-500">
+                                    <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
+                                </p>
+                                ) : (
+                                <div className="mt-1 flex items-center gap-x-1.5">
+                                    <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                                    </div>
+                                    <p className="text-xs leading-5 text-gray-500">Online</p>
+                                </div>
+                                )}
+                            </div>
+                            </li>
+                        ))}
+                    </ul>
                 </main>
             </div>
-            <aside className='z-20 hidden w-64 overflow-y-auto bg-white dark:bg-sky-100   md:block flex-shrink-0' >
+            <aside className='z-20 hidden w-64 overflow-y-auto bg-white dark:bg-sky-100 md:block flex-shrink-0 sticky' >
                 
                 <div className='py-4 text-black dark:text-black'>
 
@@ -40,12 +183,12 @@ export default function Dashboard(){
                             <h1>PRIVO</h1>
                         </a> */}
                         
-                    <ul class="mt-6">
+                    <ul className="mt-6">
                         <li className="relative px-6 py-3">
                         <span className="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg" aria-hidden="true"></span>
                         <a className="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-blue-800 dark:hover:text-blue-200 dark:text-blue-400" href="/dashboard">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                             </svg>
                             <span className="ml-4">Home</span>
                         </a>
@@ -55,7 +198,7 @@ export default function Dashboard(){
                         <li className="relative px-6 py-3">
                         <a className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-blue-400" href="/files">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
                             </svg>
                             <span className="ml-4">Files</span>
                         </a>
@@ -63,7 +206,7 @@ export default function Dashboard(){
                         <li className="relative px-6 py-3">
                         <a className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-blue-400" href="/team">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
                             </svg>
                             <span className="ml-4">Team</span>
                         </a>
@@ -71,7 +214,7 @@ export default function Dashboard(){
                         <li className="relative px-6 py-3">
                         <a className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-blue-400" href="#">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
                             </svg>
                             <span className="ml-4">Archive</span>
                         </a>
